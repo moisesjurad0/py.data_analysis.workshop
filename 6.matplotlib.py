08345af11987b012024098c26614ea5fe3f8384f
Moisés Jurado <moises003@gmail.com>
2023-06-19 18:40:36 -0500

    figsize

--- 6.matplotlib.py
+++ 6.matplotlib.py
@@ -13,51 +13,56 @@ print(tabla.head())
 print('---')
 
 
-# GRAPHS
-# ------
-print('---')
-ali = tabla[tabla.Categoría == 'Comestibles']
-print(ali.head())
-print('---')
+# # GRAPHS
+# # ------
+# print('---')
+# ali = tabla[tabla.Categoría == 'Comestibles']
+# print(ali.head())
+# print('---')
 
-print(ali.Artículo.value_counts().head())
-print('---')
+# print(ali.Artículo.value_counts().head())
+# print('---')
 
-ali.Artículo.value_counts().plot()  # por defecto es plot(kind='Line')
-plt.show()
-print('---')
+# ali.Artículo.value_counts().plot()  # por defecto es plot(kind='Line')
+# plt.show()
+# print('---')
 
-# por defecto es plot(kind='Line')
-ali.Artículo.value_counts().plot(kind='line', color='red')
-plt.show()
-print('---')
+# # por defecto es plot(kind='Line')
+# ali.Artículo.value_counts().plot(kind='line', color='red')
+# plt.show()
+# print('---')
 
-ali.Artículo.value_counts().plot(kind='bar')
-plt.show()
-print('---')
+# ali.Artículo.value_counts().plot(kind='bar')
+# plt.show()
+# print('---')
 
-ali.Artículo.value_counts().plot(kind='barh')
-plt.show()
-print('---')
+# ali.Artículo.value_counts().plot(kind='barh')
+# plt.show()
+# print('---')
 
-ali.Artículo.value_counts().plot(kind='pie')
-plt.show()
-print('---')
+# ali.Artículo.value_counts().plot(kind='pie')
+# plt.show()
+# print('---')
 
 
-# Dispersion Ghraphics
-tabla.head().plot(kind='scatter', x='Unidades', y='Ganancia')
-plt.show()
-print('---')
+# # Dispersion Ghraphics
+# tabla.head().plot(kind='scatter', x='Unidades', y='Ganancia')
+# plt.show()
+# print('---')
 
-# Colores. Usa el parametro color
-tabla.head().plot(kind='scatter', x='Unidades', y='Ganancia', color='green')
-plt.show()
-print('---')
+# # Colores. Usa el parametro color
+# tabla.head().plot(kind='scatter', x='Unidades', y='Ganancia', color='green')
+# plt.show()
+# print('---')
 
-# colormap. You can change colormap instead of color with some default
-# values as e.g. jet & twilight
-ali.Artículo.value_counts().plot(kind='pie', colormap='twilight')
+# # colormap. You can change colormap instead of color with some default
+# # values as e.g. jet & twilight
+# ali.Artículo.value_counts().plot(kind='pie', colormap='twilight')
+# plt.show()
+# print('---')
+
+# sizes, use figsize to proportionally change sizes.
+tabla.Artículo.value_counts().plot(kind='pie', colormap='twilight', figsize=(20, 10))
 plt.show()
 print('---')
 
