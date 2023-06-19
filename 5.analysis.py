da64bdc2a1b565d7d95043e2e928152b76dd1f61
Moisés Jurado <moises003@gmail.com>
2023-06-19 14:11:03 -0500

    strings

--- 5.analysis.py
+++ 5.analysis.py
@@ -8,8 +8,10 @@ tabla = pd.read_csv(r'ventasTotales.csv', skiprows=5, sep=';')
 tabla.info()
 print(tabla.head())
 
+
 # COUNT
 # ------
+
 # counts ocurrences in a Column (Column "Vendedor") - Descendent
 print(tabla.Vendedor.value_counts())
 
@@ -21,6 +23,7 @@ print(tabla.Vendedor.value_counts(dropna=True))
 
 # SORT
 # ------
+
 # ordena una columna
 print(tabla.Vendedor.sort_values())
 
@@ -30,6 +33,7 @@ print(tabla.sort_values(by=['Vendedor', 'Ganancia']))
 
 # INDEXING
 # ---------
+
 # filtra columna por valor
 print(tabla.Artículo == 'Gorras')
 # filtra tabla por columna filtrada
@@ -39,7 +43,11 @@ print(tabla[(tabla.Artículo == 'Gorras') & (tabla.Región == 'Norte')])
 
 # STRINGS
 # ---------
-print("TMP1----------")
 
+# filtra columna por valor, pero manejandola como un str
+print(tabla.Vendedor.str.contains('Trevor'))
+# filtra tabla por columna filtrada
+print("TMP1----------")
+print(tabla[tabla.Vendedor.str.contains('Trevor')])
 
 print(pd.__version__)
