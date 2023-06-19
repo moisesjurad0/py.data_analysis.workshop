3ced893a4024ca5480a9e97c0c639edbc1aee8c9
Moisés Jurado <moises003@gmail.com>
2023-06-19 13:24:55 -0500

    shape, head y tail

--- 4.pandas.review.visualize.py
+++ 4.pandas.review.visualize.py
@@ -3,11 +3,29 @@
 import pandas as pd
 
 # skip 5 first rows from .csv file
-tabla = pd.read_csv(r'ventasTotales.csv', skiprows=5)
+tabla = pd.read_csv(r'ventasTotales.csv', skiprows=5, sep=';')
 
 # pd.set_option("display.max.columns", None)
 print(tabla.to_string(max_rows=6))
+
 print(type(tabla))  # <class 'pandas.core.frame.DataFrame'>
 
+# cantidad de filas y columnas
+# -----------------------------
+# debes configurar el separador del DataFrame "sep" si tienes un separador
+# que no es coma, sino no cogerá las columnas.
+print(tabla.shape)
+
+# por defecto muestra las primeras 5 filas
+print(tabla.head())
+
+# tb puede recibir un parametro de cantidad de filas.
+print(tabla.head(3))
+
+# por defecto muestra las ULTIMAS 5 filas
+print(tabla.tail())
+
+# por defecto muestra las ULTIMAS 5 filas
+print(tabla.tail(3))
 
 print(pd.__version__)
