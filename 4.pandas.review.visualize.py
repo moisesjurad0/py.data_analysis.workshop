43fff0c01a1b78af482fcc942336b52559814357
Moisés Jurado <moises003@gmail.com>
2023-06-19 12:55:01 -0500

    skiprows update

--- 4.pandas.review.visualize.py
+++ 4.pandas.review.visualize.py
@@ -4,7 +4,10 @@ import pandas as pd
 
 # skip 5 first rows from .csv file
 tabla = pd.read_csv(r'ventasTotales.csv', skiprows=5)
-print(tabla)
-type(tabla)  # <class 'pandas.core.frame.DataFrame'>
+
+# pd.set_option("display.max.columns", None)
+print(tabla.to_string(max_rows=6))
+print(type(tabla))  # <class 'pandas.core.frame.DataFrame'>
+
 
 print(pd.__version__)
