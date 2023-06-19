1b7de71958cd3464a92b24a89fa69a8096fe62bb
Moisés Jurado <moises003@gmail.com>
2023-06-19 13:35:44 -0500

    count by column

--- 5.analysis.py
+++ 5.analysis.py
@@ -8,6 +8,10 @@ tabla = pd.read_csv(r'ventasTotales.csv', skiprows=5, sep=';')
 tabla.info()
 print(tabla.head())
 
+# counts ocurrences in a Column (Column "Vendedor") - Descendent
+print(tabla.Vendedor.value_counts())
 
+# counts ocurrences in a Column (Column "Vendedor") - Ascendent
+print(tabla.Vendedor.value_counts(ascending=True))
 
 print(pd.__version__)
