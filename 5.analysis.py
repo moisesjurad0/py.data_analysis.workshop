aea567f58eacc1a69b8ea58f6a422e28e8739f8a
Moisés Jurado <moises003@gmail.com>
2023-06-19 13:53:37 -0500

    sorting

--- 5.analysis.py
+++ 5.analysis.py
@@ -8,6 +8,8 @@ tabla = pd.read_csv(r'ventasTotales.csv', skiprows=5, sep=';')
 tabla.info()
 print(tabla.head())
 
+# COUNT
+# ------
 # counts ocurrences in a Column (Column "Vendedor") - Descendent
 print(tabla.Vendedor.value_counts())
 
@@ -16,5 +18,17 @@ print(tabla.Vendedor.value_counts(ascending=True))
 
 # use "dropna" to drop nulls so that not count them
 print(tabla.Vendedor.value_counts(dropna=True))
+
+# SORT
+# ------
+# ordena una columna
+print(tabla.Vendedor.sort_values())
+
+print("TMP------TMP")
+# ordena toda la tabla, se usa by para determinar que columnas
+# por defecto es ASCendet
+print(tabla.sort_values(by=['Vendedor', 'Ganancia']))
+
+# INDEXING
 
 print(pd.__version__)
