20a8ce0ace715572d532a843c0e4c924ee8da66a
Moisés Jurado <moises003@gmail.com>
2023-06-19 18:29:43 -0500

    graphics

--- 6.matplotlib.py
+++ 6.matplotlib.py
@@ -6,16 +6,47 @@ import pandas as pd
 # skip 5 first rows from .csv file
 tabla = pd.read_csv(r'ventasTotales.csv', skiprows=5, sep=';')
 
+print('---info---')
 tabla.info()
+print('---initial table head---')
 print(tabla.head())
+print('---')
 
 
 # GRAPHS
 # ------
+print('---')
 ali = tabla[tabla.Categoría == 'Comestibles']
-ali.head()
-ali.Artículo.value_counts()
-ali.Artículo.value_counts().plot()  # por defecto es plot(kind='Line')
+print(ali.head())
+print('---')
 
+print(ali.Artículo.value_counts().head())
+print('---')
+
+ali.Artículo.value_counts().plot()  # por defecto es plot(kind='Line')
+plt.show()
+print('---')
+
+ali.Artículo.value_counts().plot(kind='line')  # por defecto es plot(kind='Line')
+plt.show()
+print('---')
+
+ali.Artículo.value_counts().plot(kind='bar')
+plt.show()
+print('---')
+
+ali.Artículo.value_counts().plot(kind='barh')
+plt.show()
+print('---')
+
+ali.Artículo.value_counts().plot(kind='pie')
+plt.show()
+print('---')
+
+
+# Dispersion Ghraphics
+tabla.head().plot(kind='scatter', x='Unidades', y='Ganancia')
+plt.show()
+print('---')
 
 print(pd.__version__)
