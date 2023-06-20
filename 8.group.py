ea5d00c1686beeb90d81591ef8a049542b7088e2
Moisés Jurado <moises003@gmail.com>
2023-06-19 22:16:44 -0500

    8.GROUP BY

--- 8.group.py
+++ 8.group.py
@@ -2,9 +2,23 @@ import pandas as pd
 tabla = pd.read_csv(r'ventasTotales.csv', skiprows=5, sep=';')
 print(tabla.head())
 print('---')
+print('---')
+print('---')
 
 # GROUP BY
 # creates a group of DataFrames from a DataFrame
 tabla.groupby('Región')  # <pandas.core.groupby.generic.DataFrameGroupBy
-
 print(list(tabla.groupby('Región')))
+print('---')
+print('---')
+print('---')
+
+# ITERATE BY GROUP BY
+for group_key, group_value in tabla.groupby('Región'):
+    print(group_key)
+    print(group_value)
+print('---')
+print('---')
+print('---')
+print(type(group_key))  # str
+print(type(group_value))  # pandas.core.frame.DataFrame
