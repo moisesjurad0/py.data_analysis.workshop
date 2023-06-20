9259449d0582a9b1c3835fa4fae1e0875cc7556b
Moisés Jurado <moises003@gmail.com>
2023-06-19 23:48:49 -0500

    unstack

--- 9.format.py
+++ 9.format.py
@@ -6,3 +6,36 @@ print('---')
 print('---')
 
 # STACK
+
+cat = tabla[
+    (tabla.Vendedor == 'Sarah Bond') &
+    (
+        tabla.Categoría.str.contains('Vestimenta|Comestibles')
+    ) |
+    (
+        (tabla.Categoría == 'Vestimenta') |
+        (tabla.Categoría == 'Comestibles')
+    )
+]
+print(cat)
+print('---')
+print('---')
+print('---')
+
+c = cat.groupby(['Artículo', 'Categoría', 'Región']).size()
+print(c)
+print('---')
+print('---')
+print('---')
+
+d = c.unstack('Categoría')
+print(d)
+print('---')
+print('---')
+print('---')
+
+e = c.unstack('Región')
+print(e)
+print('---')
+print('---')
+print('---')
